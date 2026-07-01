c6138449aea79643ab25c9ae63d5c610dff9995d
ehambrook <167354651+ehambrook@users.noreply.github.com>
2026-07-01 12:48:59 -0700

    Developer Skills Section

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+
+// Setting up Prettier and VS Code Lecture
+const x = 23;
+
+if (x === 23) console.log(23);
+
+const calcAge = (birthYear) => 2037 - birthYear;
+
+console.log(calcAge(1991));
