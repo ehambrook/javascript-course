e4457b30b9fa80f3e0e2bfc128018e447b7ff0db
ehambrook <167354651+ehambrook@users.noreply.github.com>
2026-07-06 16:27:23 -0700

    Section 7 Guess My Number project

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,64 @@
 'use strict';
+
+function decreaseScore(message) {
+    document.querySelector(".message").textContent = message;
+    score--;
+    document.querySelector(".score").textContent = score;
+}
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+document.querySelector(".score").textContent = score;
+
+document.querySelector(".check").addEventListener("click", function() {
+    // const guess = Number(document.querySelector('.guess').value);
+    
+    if (document.querySelector('.guess').value === "") {
+        document.querySelector(".message").textContent = "⛔ No number!";
+    } else {
+        const guess = Number(document.querySelector('.guess').value);
+    }
+
+    if (guess === secretNumber) {
+        document.querySelector(".message").textContent = "🎉 Correct number!";
+        document.querySelector(".number").textContent = secretNumber;
+
+        document.querySelector("body").style.backgroundColor = "#60b347";
+        document.querySelector(".number").style.width = "30rem";
+
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector(".highscore").textContent = highScore;
+        }
+    }
+
+    if (score === 1 && guess != secretNumber) {
+        decreaseScore("🥀 You lost the game");
+    }
+
+    if (score > 1 && guess > secretNumber) {
+        decreaseScore("📈 Too high!")
+    }
+
+    if (score > 1 && guess < secretNumber) {
+        decreaseScore("📉 Too low!");
+    }
+})
+
+/////////////////////////////////////////////////////////
+// Coding Challenge #1: implement the game reset functionality
+
+document.querySelector(".again").addEventListener("click", function() {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    score = 20;
+
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".guess").value = "";
+    document.querySelector(".message").textContent = "Start guessing...";
+    document.querySelector(".number").textContent = "?";
+
+    document.querySelector(".number").style.width = "15rem";
+    document.querySelector("body").style.backgroundColor = "#222";
+})
