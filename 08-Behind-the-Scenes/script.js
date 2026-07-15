6a4361efd4eec0d2a49ded5e9637d7a176b3c96c
ehambrook <167354651+ehambrook@users.noreply.github.com>
2026-07-15 15:42:13 -0700

    Section 8 this Keyword and Memory Management

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // Scoping in Practice lecture
 
 function calcAge(birthYear) {
@@ -90,3 +91,136 @@ console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
 
+
+
+// The this Keyword Lecture
+
+console.log(this);
+
+const calcAge = function(birthYear) {
+    console.log(2037 - birthYear);
+    console.log(this);   // this undefined here, because strict mode used
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+    console.log(2037 - birthYear);
+    console.log(this);   // this is window, keyword for parent scope
+};
+calcAgeArrow(1980);
+
+const jonas = {
+    year: 1991,
+    calcAge: function() {
+        console.log(this);
+        console.log(2037 - this.year);
+    },
+};
+jonas.calcAge();
+
+const matilda = {
+    year: 2017,
+};
+
+// Method borrowing
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();   // Undefined, because f is not attached to an object
+
+
+
+// Regular Functions vs Arrow Functions Lecture
+
+// var firstName = "Matilda";   // Creates property on global object
+
+const jonas = {
+    firstName: "Jonas",
+    year: 1991,
+    calcAge: function() {
+        // console.log(this);
+        console.log(2037 - this.year);
+
+        // Solution 1, create self in parent scope to preserve this keyword
+        // const self = this;   // self or that
+        // const isMillenial = function() {
+        //     console.log(self);
+        //     console.log(self.year >= 1981 && self.year <= 1996);
+        // };
+        // isMillenial();
+
+        // Solution 2, use arrow function to inherit the this keyword from parent scope
+        const isMillenial = () => {
+            console.log(this);
+            console.log(this.year >= 1981 && this.year <= 1996);
+        };
+        isMillenial();
+    },
+
+    greet: () => console.log(`Hey ${this.firstName}`),   // this.firstName undefined in arrow function
+};
+jonas.greet();
+console.log(this.firstName);   // Also undefined
+
+jonas.calcAge();
+
+// arguments keyword
+const addExpr = function(a, b) {
+    console.log(arguments)
+    return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+    console.log(arguments);   // arguments not defined
+    return a + b;
+};
+addArrow(2, 5, 8);
+*/
+
+
+// Memory Management Lecture
+
+const jessica1 = {
+    firstName: "Jessica",
+    lastName: "Williams",
+    age: 27,
+};
+
+function marryPerson(originalPerson, newLastName) {
+    originalPerson.lastName = newLastName;
+    return originalPerson;
+};
+const marriedJessica = marryPerson(jessica1, "Davis");
+
+// const marriedJessica = jessica;
+// marriedJessica.lastName = "Davis";
+
+console.log("Before:", jessica1);
+console.log("After:", marriedJessica);
+
+const jessica = {
+    firstName: "Jessica",
+    lastName: "Williams",
+    age: 27,
+    family: ["Alice", "Bob"],   // Nested object, has its own memory address
+};
+
+// Shallow copy
+const jessicaCopy = {...jessica};
+jessicaCopy.lastName = "Davis";
+
+// jessicaCopy.family.push("Carol");   // Changes array in both ojects
+// jessicaCopy.family.push("Dave");
+// console.log("Before:", jessica);
+// console.log("After:", jessicaCopy);
+
+// Deep copy/clone
+const jessicaClone = structuredClone(jessica);
+jessicaClone.family.push("Carol");
+jessicaClone.family.push("Dave");
+
+console.log("Original:", jessica);
+console.log("Clone:", jessicaClone);
